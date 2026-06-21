895469cf41c95ba884c9f2feacb55dc4d4e4da9e
Bader Al Refai <badermajidalrefai@gmail.com>
2026-06-21 12:02:07 +0300

    tweaked the pattern

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import re
 
 def extract_cards(md):
-    pattern = r"\*\*(.*?)\*\*:\s(.*)"
+    pattern = r"\s?-?\s?\*\*(.*?):\*\*\s(.*)"
     questions = []
 
     for line in md:
@@ -11,5 +11,9 @@ def extract_cards(md):
 
     return questions
 
-def csv_conversion(cards_dict):
+def csv_conversion(cards_array):
+    with open('cards.csv', 'w') as file:
+        for question, answer in cards_array:
+            file.write(f"{question}, {answer}\n")
+            
     return
