0e1a1ea2e34a913c72b62da471ed01283a82ba49
Bader Al Refai <badermajidalrefai@gmail.com>
2026-06-23 23:40:36 +0300

    Made function to gather anki state

---
+++ sync.py
@@ -0,0 +1,79 @@
+import requests
+def sync_to_anki():
+
+    return 
+
+def get_current_anki_decks(anki_port):
+    current_anki_decks = requests.post(
+        anki_port,
+        json={
+            "action" : "deckNamesAndIds",
+            "version" : 6
+        }
+    )
+    current_anki_decks = current_anki_decks.json()
+    current_anki_decks = current_anki_decks["result"]
+
+    return current_anki_decks
+
+def setup_anki_state(current_anki_decks):
+    anki_state = {}
+
+    for deck_name, deck_id in current_anki_decks.items():
+        anki_state[deck_name] = {
+            "id" : deck_id,
+            "cards" : {}
+        }
+
+    return anki_state
+
+def get_cards(anki_port, deck):
+    cards = requests.post(
+        anki_port, json={
+            "action" : "findCards",
+            "version" : 6,
+            "params": {
+                "query": f'deck:"{deck}"'
+            }
+        }
+    )
+    cards = cards.json()
+
+    return cards
+
+def get_cards_details(anki_port, cards):
+    cards_details = requests.post(
+        anki_port,json={
+            "action" : "cardsInfo",
+            "version" : 6,
+            "params" : {
+                "cards" : cards["result"]
+            }
+        }
+    )
+    cards_details = cards_details.json()
+    cards_details = cards_details["result"]
+
+    return cards_details
+
+def add_cards_to_anki_state(anki_state, deck, cards_details):
+    for details in cards_details:
+        card_id = details["cardId"]
+        anki_state[deck]["cards"][card_id] = {
+            "front_side" : details["fields"]["Front"],
+            "back_side" : details["fields"]["Back"]
+        }
+
+def get_anki_state(anki_port):
+    current_anki_decks = get_current_anki_decks(anki_port)
+    anki_state = setup_anki_state(current_anki_decks)
+
+    for deck in anki_state:
+        cards = get_cards(anki_port, deck)
+        cards_details = get_cards_details(anki_port, cards)
+        add_cards_to_anki_state(anki_state, deck, cards_details)
+        
+    return anki_state
+
+def get_obsidian_state():
+    return
