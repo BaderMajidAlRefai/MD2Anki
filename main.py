a0686002c92c9fcf18d84c9e991caef2d2048731
Bader Al Refai <badermajidalrefai@gmail.com>
2026-06-23 05:02:11 +0300

    made changes to JSON file

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import json
 from functions import csv_conversion, extract_cards
 
+with open("settings.json", "r") as settings_file:
+    settings = json.load(settings_file)
+
+print (settings)
 with open("test.md", "r") as md:
     csv_conversion(extract_cards(md))
