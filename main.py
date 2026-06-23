4b200d1957579fd795d851f19888e7b3f6fcb8bf
Bader Al Refai <badermajidalrefai@gmail.com>
2026-06-23 07:32:14 +0300

    imported a different file I'm still working on

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
 from parsing import csv_conversion, extract_cards
+from sync import *
 
 with open("settings.json", "r") as settings_file:
     settings = json.load(settings_file)
 
 with open("test.md", "r") as md:
     csv_conversion(extract_cards(md, settings))
+
+print(get_anki_state(settings["anki"]["anki_connect_port"]))
