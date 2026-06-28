2a141962977e925b4e3b60040453e0bd590208c8
Bader Al Refai <badermajidalrefai@gmail.com>
2026-06-29 00:53:11 +0300

    removed csv conversion function

--- parsing.py
+++ parsing.py
@@ -11,8 +11,3 @@ def extract_cards(md, settings):
 
     return questions
 
-def csv_conversion(cards_array):
-    with open('cards.csv', 'w') as file:
-        for question, answer in cards_array:
-            file.write(f"{question}, {answer}\n")
-    return 0
