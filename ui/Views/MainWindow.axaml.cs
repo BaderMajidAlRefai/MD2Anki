03343c1cdde21ba28ec2ca38499db1d0a691d003
Bader Al Refai <badermajidalrefai@gmail.com>
2026-07-28 13:30:30 +0300

    saving just in case my storage corrupts during my roadtrip

---
+++ ui/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ui.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
